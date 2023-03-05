0b5640879df977801d6120eb1fedd572a055c025
TrongDuc <ducb1809230@student.ctu.edu.vn>
2023-03-05 18:32:11 +0700

    Install prettier, lint-staged, husky

--- src/app/controllers/NewsController.js
+++ src/app/controllers/NewsController.js
@@ -1,5 +1,4 @@
 class NewsController {
-
     // [GET] /news
     index(req, res) {
         res.render('news');
@@ -7,8 +6,8 @@ class NewsController {
 
     // [GET] /news/:slug
     show(req, res) {
-        res.send('New message')
+        res.send('New message');
     }
 }
 
-module.exports = new NewsController;
+module.exports = new NewsController();

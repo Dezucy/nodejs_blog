fc80966257f174b8018fc4faf8b5c8fb51acadb2
TrongDuc <ducb1809230@student.ctu.edu.vn>
2023-03-05 18:45:11 +0700

    test husky 3

--- src/index.js
+++ src/index.js
@@ -30,8 +30,8 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 //ROUTEs init
-      route(app);
+      route(      app);
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`);
+    });
